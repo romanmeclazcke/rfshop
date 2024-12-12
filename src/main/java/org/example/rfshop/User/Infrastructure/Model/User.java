6a1c883ae3ce90ebba4f7e9218535567e168473e
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-12 17:11:54 -0300

    feat: Added attributes to User entity

--- src/main/java/org/example/rfshop/User/Infrastructure/Model/User.java
+++ src/main/java/org/example/rfshop/User/Infrastructure/Model/User.java
@@ -16,4 +16,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne()
+    private Role role;
+
 }
