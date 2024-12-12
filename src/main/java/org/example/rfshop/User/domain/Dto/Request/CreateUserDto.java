e958a2a44dccdee83652dd0b75e054d22f1e29f2
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-12 17:11:31 -0300

    feat: Added attributes and validators to CrateUserDto

--- src/main/java/org/example/rfshop/User/domain/Dto/Request/CreateUserDto.java
+++ src/main/java/org/example/rfshop/User/domain/Dto/Request/CreateUserDto.java
@@ -1,4 +1,29 @@
 package org.example.rfshop.User.domain.Dto.Request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserDto {
+
+    @Email(message = "The email should be valid")
+    @NotEmpty(message = "The email cannot be empty")
+    private String email;
+
+    @NotEmpty(message = "The username cannot be empty")
+    @Size(min = 3, max = 50, message = "The username must be between 3 and 50 characters")
+    private String userName;
+
+    @NotEmpty(message = "The last name cannot be empty")
+    private String lastName;
+
+    @NotEmpty(message = "The password cannot be empty")
+    @Size(min = 6, message = "The password must be at least 6 characters long")
+    private String password;
 }
