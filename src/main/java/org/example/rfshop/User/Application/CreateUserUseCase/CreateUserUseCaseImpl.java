b48c8310dd6ce7efba24e0e9a60ced926f5e86b3
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-12 17:09:15 -0300

    feat: Implemented method to create user

--- src/main/java/org/example/rfshop/User/Application/CreateUserUseCase/CreateUserUseCaseImpl.java
+++ src/main/java/org/example/rfshop/User/Application/CreateUserUseCase/CreateUserUseCaseImpl.java
@@ -1,15 +1,33 @@
 package org.example.rfshop.User.Application.CreateUserUseCase;
 
+import org.example.rfshop.User.Infrastructure.Mapper.UserMapper;
+import org.example.rfshop.User.Infrastructure.Model.User;
+import org.example.rfshop.User.Infrastructure.Repository.UserRepository;
 import org.example.rfshop.User.domain.Dto.Request.CreateUserDto;
 import org.example.rfshop.User.domain.Dto.Response.UserResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class CreateUserUseCaseImpl implements CreateUserUseCase{
+
+    private UserRepository userRepository;
+    private UserMapper userMapper;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public CreateUserUseCaseImpl(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public UserResponseDto createUser(CreateUserDto createUserDto) {
-        System.out.println("Hello");
-        return null;
+        User user = userMapper.toEntity(createUserDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userMapper.toDto(userRepository.save(user));
     }
 }
