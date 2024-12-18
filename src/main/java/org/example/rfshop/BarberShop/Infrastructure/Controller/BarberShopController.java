5944122e776b587d6fde2dc95e1b5e6cd407c27e
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-17 22:33:29 -0300

    refactor: changed request mapping to barber shop

--- src/main/java/org/example/rfshop/BarberShop/Infrastructure/Controller/BarberShopController.java
+++ src/main/java/org/example/rfshop/BarberShop/Infrastructure/Controller/BarberShopController.java
@@ -2,6 +2,8 @@ package org.example.rfshop.BarberShop.Infrastructure.Controller;
 
 import jakarta.validation.Valid;
 import org.example.rfshop.BarberShop.Application.CreateBarberShopUseCase.CreateBarberShopUseCase;
+import org.example.rfshop.BarberShop.Application.DeleteBarberShopUseCase.DeleteBarberShop;
+import org.example.rfshop.BarberShop.Application.DeleteBarberShopUseCase.DeleteBarberShopImpl;
 import org.example.rfshop.BarberShop.Application.GetAllBarberShopUseCase.GetAllBarberShopUseCase;
 import org.example.rfshop.BarberShop.Application.GetBarberShopById.GetBarberShopByIdUseCase;
 import org.example.rfshop.BarberShop.Application.UpdateBarberShopUseCase.UpdateBarberShopUseCase;
@@ -10,24 +12,25 @@ import org.example.rfshop.BarberShop.Domain.Dto.Request.UpdateBarberShopDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/barber-shop")
+@RequestMapping("/barber-shops")
 public class BarberShopController {
 
     private final CreateBarberShopUseCase createBarberShopUseCase;
     private final UpdateBarberShopUseCase updateBarberShopUseCase;
     private final GetAllBarberShopUseCase getAllBarberShopUseCase;
     private final GetBarberShopByIdUseCase getBarberShopByIdUseCase;
+    private final DeleteBarberShop deleteBarberShop;
 
     @Autowired
-    public BarberShopController(CreateBarberShopUseCase createBarberShopUseCase, UpdateBarberShopUseCase updateBarberShopUseCase, GetAllBarberShopUseCase getAllBarberShopUseCase, GetBarberShopByIdUseCase getBarberShopByIdUseCase) {
+    public BarberShopController(CreateBarberShopUseCase createBarberShopUseCase, UpdateBarberShopUseCase updateBarberShopUseCase, GetAllBarberShopUseCase getAllBarberShopUseCase, GetBarberShopByIdUseCase getBarberShopByIdUseCase, DeleteBarberShop deleteBarberShop ) {
         this.createBarberShopUseCase = createBarberShopUseCase;
         this.updateBarberShopUseCase = updateBarberShopUseCase;
         this.getAllBarberShopUseCase = getAllBarberShopUseCase;
         this.getBarberShopByIdUseCase = getBarberShopByIdUseCase;
+        this.deleteBarberShop = deleteBarberShop;
     };
 
 
@@ -46,9 +49,14 @@ public class BarberShopController {
         return new ResponseEntity<>(this.createBarberShopUseCase.execute(ownerId,createBarberShopDto), HttpStatus.CREATED);
     }
 
-
     @PatchMapping("/{barberId}")
     public ResponseEntity<?> updateBarberShop(@PathVariable Long barberId,@RequestBody @Valid UpdateBarberShopDto updateBarberShopDto) {
         return new ResponseEntity<>(this.updateBarberShopUseCase.execute(barberId,updateBarberShopDto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{barberId}")
+    public ResponseEntity<?> deleteBarberById(@PathVariable Long barberId) {
+        this.deleteBarberShop.execute(barberId);
+        return new ResponseEntity<>("Barber deleted", HttpStatus.OK);
     }
 }
