506b187ab25671e545a9928bd186b73893a15842
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-22 14:53:25 -0300

    feat: Implemented post controller

--- src/main/java/org/example/rfshop/Post/Infrastructure/Controller/PostController.java
+++ src/main/java/org/example/rfshop/Post/Infrastructure/Controller/PostController.java
@@ -1,7 +1,6 @@
 package org.example.rfshop.Post.Infrastructure.Controller;
 
 import org.example.rfshop.Post.Application.CreatePostUseCase.CreatePostUseCase;
-import org.example.rfshop.Post.Application.CreatePostUseCase.CreatePostUseCaseImpl;
 import org.example.rfshop.Post.Application.DeletePostUseCase.DeletePostUseCase;
 import org.example.rfshop.Post.Application.GetPostByBarberShopIdUseCase.GetPostByBarberShopIdUseCase;
 import org.example.rfshop.Post.Application.GetPostByIdUseCase.GetPostByIdUseCase;
@@ -9,8 +8,10 @@ import org.example.rfshop.Post.Application.UpdatePostUseCase.UpdatePostUseCase;
 import org.example.rfshop.Post.Domain.Dto.Request.CreatePostDto;
 import org.example.rfshop.Post.Domain.Dto.Request.UpdatePostDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/posts")
@@ -33,28 +34,32 @@ public class PostController {
 
     @GetMapping("/barber-shop/{barberId}")
     public ResponseEntity<?> getAllPostByBarberShopId(@PathVariable Long barberId) {
-      return null;
+        return new ResponseEntity<>(this.getPostByBarberShopIdUseCase.execute(barberId), HttpStatus.OK);
     }
 
     @GetMapping("/{postId}")
     public ResponseEntity<?> getPostById(@PathVariable Long postId) {
-        return null;
+        return new ResponseEntity<>(this.getPostByIdUseCase.execute(postId), HttpStatus.OK);
     }
 
     @PostMapping("/barber-shop/{barberId}")
-    public ResponseEntity<?> createPost(@PathVariable Long barberId, @RequestBody CreatePostDto createPostDto) {
-        return null;
+    public ResponseEntity<?> createPost(@PathVariable Long barberId,
+                                        @RequestParam("image") MultipartFile image,
+                                        @RequestParam("description") String description) {
+        CreatePostDto createPostDto = new CreatePostDto(image, description);
+        return new ResponseEntity<>(this.createPostUseCase.execute(barberId, createPostDto), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{postId}")
     public ResponseEntity<?> updatePost(@PathVariable Long postId, @RequestBody UpdatePostDto updatePostDto) {
-        return null;
+        return new ResponseEntity<>(this.updatePostUseCase.execute(postId, updatePostDto), HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{postId}")
     public ResponseEntity<?> deletePost(@PathVariable Long postId) {
-        return null;
+        this.deletePostUseCase.execute(postId);
+        return new ResponseEntity<>("Post with id "+postId+ " deleted", HttpStatus.OK);
     }
 
 }
