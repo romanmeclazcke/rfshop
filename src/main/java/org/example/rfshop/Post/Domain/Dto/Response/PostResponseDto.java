526663a085bf0ca6d6270ed5fe79da720e1031b6
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-22 14:54:55 -0300

    feat: Update post response to add post id and date of create

--- src/main/java/org/example/rfshop/Post/Domain/Dto/Response/PostResponseDto.java
+++ src/main/java/org/example/rfshop/Post/Domain/Dto/Response/PostResponseDto.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostResponseDto {
+    private Long id;
     private String secureUrl;
     private String description;
+    private Date createdAt;
 
 }
