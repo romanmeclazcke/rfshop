b975c631205c5bc6b52214c14d2d7c2d49cc6f6c
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-22 14:54:02 -0300

    fix: Resolved error on post repository (change "Post P" to "Post p")

--- src/main/java/org/example/rfshop/Post/Infrastructure/Repository/PostRepository.java
+++ src/main/java/org/example/rfshop/Post/Infrastructure/Repository/PostRepository.java
@@ -3,12 +3,14 @@ package org.example.rfshop.Post.Infrastructure.Repository;
 import org.example.rfshop.Post.Infrastructure.Model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
-    @Query("SELECT p FROM Post P WHERE p.barberShop.id = :barberId")
-    List<Post> findAllByBarberShopId(Long barberId);
+
+    @Query("SELECT p FROM Post p WHERE p.barberShop.id = :barberId")
+    List<Post> findAllByBarberShopId(@Param("barberId") Long barberId);
 }
