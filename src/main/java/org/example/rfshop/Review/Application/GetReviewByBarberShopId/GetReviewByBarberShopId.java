d4e6910eb2270882009c4d3ee4d4eff6438ba197
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-12-24 15:17:20 -0300

    feat: i Implemented get review by barber shop id

--- src/main/java/org/example/rfshop/Review/Application/GetReviewByBarberShopId/GetReviewByBarberShopId.java
+++ src/main/java/org/example/rfshop/Review/Application/GetReviewByBarberShopId/GetReviewByBarberShopId.java
@@ -1,4 +1,8 @@
 package org.example.rfshop.Review.Application.GetReviewByBarberShopId;
 
+import org.example.rfshop.Review.Domain.Dto.Response.ReviewResponseDto;
+import java.util.List;
+
 public interface GetReviewByBarberShopId {
+    List<ReviewResponseDto> execute(Long barberShopId);
 }
