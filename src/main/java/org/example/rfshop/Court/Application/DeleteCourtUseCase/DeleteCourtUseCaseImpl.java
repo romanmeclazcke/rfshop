4a74cf7fb10ac813757f762543fde95d672329c8
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-01 19:35:42 -0300

    fix:  I modified the method to delete the court so that it is only possible by the owner of the barbershop

--- src/main/java/org/example/rfshop/Court/Application/DeleteCourtUseCase/DeleteCourtUseCaseImpl.java
+++ src/main/java/org/example/rfshop/Court/Application/DeleteCourtUseCase/DeleteCourtUseCaseImpl.java
@@ -1,8 +1,14 @@
 package org.example.rfshop.Court.Application.DeleteCourtUseCase;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.example.rfshop.Auth.Application.ExtractUserEmailFromSecurityContextUseCase.ExtractUserEmailFromSecurityContext;
+import org.example.rfshop.Court.Infrastructure.Model.Court;
 import org.example.rfshop.Court.Infrastructure.Repository.CourtRepository;
+import org.example.rfshop.User.Application.GetUserByEmail.GetUserByEmail;
+import org.example.rfshop.User.Infrastructure.Model.User;
+import org.example.rfshop.Utils.Exception.DeniedAction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 
@@ -10,17 +16,26 @@ import org.springframework.stereotype.Service;
 public class DeleteCourtUseCaseImpl implements DeleteCourtUseCases{
 
     private final CourtRepository courtRepository;
+    private final GetUserByEmail getUserByEmail;
+    private final ExtractUserEmailFromSecurityContext extractUserEmailFromSecurityContext;
 
     @Autowired
-    public DeleteCourtUseCaseImpl(CourtRepository courtRepository) {
+    public DeleteCourtUseCaseImpl(CourtRepository courtRepository, GetUserByEmail getUserByEmail, ExtractUserEmailFromSecurityContext extractUserEmailFromSecurityContext) {
         this.courtRepository = courtRepository;
+        this.getUserByEmail = getUserByEmail;
+        this.extractUserEmailFromSecurityContext = extractUserEmailFromSecurityContext;
     }
 
     @Override
     public void execute(Long courtId) {
-        this.courtRepository.findById(courtId).ifPresentOrElse(
-            this.courtRepository::delete,
-                ()->{ throw new EntityNotFoundException("Court with id"+ courtId+" not found");
-        });
+        Court court = this.courtRepository.findById(courtId).orElseThrow(() -> new EntityNotFoundException("Court with id" + courtId + " not found"));
+
+        User user = this.getUserByEmail.execute(this.extractUserEmailFromSecurityContext.execute(SecurityContextHolder.getContext()));
+
+        if (!user.getId().equals(court.getBarberShop().getOwner().getId())) {  //check if user that are deleting a court is  the owner of barber shop
+            throw new DeniedAction("You are not the owner of the barber shop");
+        }
+
+        this.courtRepository.delete(court);
     }
 }
