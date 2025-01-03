559fa83f4c4b6975694add04741571b16274a637
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-03 13:47:54 -0300

    fix: I update method to crate report (added logic to send email)

--- src/main/java/org/example/rfshop/Report/Application/CreateReportUseCase/CreateReportUseCaseImpl.java
+++ src/main/java/org/example/rfshop/Report/Application/CreateReportUseCase/CreateReportUseCaseImpl.java
@@ -2,6 +2,8 @@ package org.example.rfshop.Report.Application.CreateReportUseCase;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.example.rfshop.Auth.Application.ExtractUserEmailFromSecurityContextUseCase.ExtractUserEmailFromSecurityContext;
+import org.example.rfshop.Email.Application.SendEmailUseCase.SendEmailUseCase;
+import org.example.rfshop.Email.Domain.EmailTypes.EmailReport;
 import org.example.rfshop.Report.Domain.Dto.Request.CreateReportDto;
 import org.example.rfshop.Report.Domain.Dto.Response.ReportResponseDto;
 import org.example.rfshop.Report.Infrastructure.Mapper.ReportMapper;
@@ -16,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 public class CreateReportUseCaseImpl implements CreateReportUseCase{
 
@@ -24,29 +28,42 @@ public class CreateReportUseCaseImpl implements CreateReportUseCase{
     private final ReviewRepository reviewRepository;
     private final GetUserByEmail getUserByEmail;
     private final ExtractUserEmailFromSecurityContext extractUserEmailFromSecurityContext;
+    private final SendEmailUseCase sendEmailUseCase;
 
     @Autowired
-    public CreateReportUseCaseImpl(ReportRepository reportRepository, ReportMapper reportMapper, ReviewRepository reviewRepository, GetUserByEmail getUserByEmail, ExtractUserEmailFromSecurityContext extractUserEmailFromSecurityContext) {
+    public CreateReportUseCaseImpl(ReportRepository reportRepository, ReportMapper reportMapper, ReviewRepository reviewRepository, GetUserByEmail getUserByEmail, ExtractUserEmailFromSecurityContext extractUserEmailFromSecurityContext, SendEmailUseCase sendEmailUseCase) {
         this.reportRepository = reportRepository;
         this.reportMapper = reportMapper;
         this.reviewRepository = reviewRepository;
         this.getUserByEmail = getUserByEmail;
         this.extractUserEmailFromSecurityContext = extractUserEmailFromSecurityContext;
+        this.sendEmailUseCase = sendEmailUseCase;
     }
 
     @Override
     public ReportResponseDto execute(Long reviewId, CreateReportDto createReportDto) {
-        Review review = this.reviewRepository.findById(reviewId)
-                .orElseThrow(() -> new EntityNotFoundException("Review with id " + reviewId + " not found"));
-        User user = this.getUserByEmail.execute(this.extractUserEmailFromSecurityContext.execute(SecurityContextHolder.getContext()));
+        try {
+            Review review = this.reviewRepository.findById(reviewId)
+                    .orElseThrow(() -> new EntityNotFoundException("Review with id " + reviewId + " not found"));
+            User user = this.getUserByEmail.execute(this.extractUserEmailFromSecurityContext.execute(SecurityContextHolder.getContext()));
 
-        if(!user.getId().equals(review.getBarberShop().getOwner().getId())){
-            throw  new DeniedAction("Only the owner of barber shop can report a review");
+            if (!user.getId().equals(review.getBarberShop().getOwner().getId())) {
+                throw new DeniedAction("Only the owner of barber shop can report a review");
+            }
+
+            Report report = this.reportMapper.toEntity(createReportDto);
+            report.setReview(review);
+            this.sendEmailUseCase.execute(new EmailReport("romanmeclazcke1234@gmail.com", "Report", review));
+            return this.reportMapper.toDto(this.reportRepository.save(report));
+        } catch (EntityNotFoundException | DeniedAction e) {
+            // Manejo de excepciones específicas
+            throw e;
+        } catch (MessagingException e) {
+            // Manejo de excepciones de correo electrónico
+            throw new RuntimeException("Error sending email", e);
+        } catch (Exception e) {
+            // Manejo de cualquier otra excepción
+            throw new RuntimeException("An unexpected error occurred", e);
         }
-
-        Report report = this.reportMapper.toEntity(createReportDto);
-        report.setReview(review);
-
-        return this.reportMapper.toDto(this.reportRepository.save(report));
     }
 }
