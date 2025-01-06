bdaf7d87532448831428b1d1516b9e15a3f03d64
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-06 11:13:41 -0300

    feat: I update auth controller to implement change password method

--- src/main/java/org/example/rfshop/Auth/Infrastructure/Controller/AuthController.java
+++ src/main/java/org/example/rfshop/Auth/Infrastructure/Controller/AuthController.java
@@ -1,29 +1,36 @@
 package org.example.rfshop.Auth.Infrastructure.Controller;
 
+import org.example.rfshop.Auth.Application.ChangePassword.ChangePasswordUseCase;
 import org.example.rfshop.Auth.Application.LoginUseCase.LoginUseCase;
+import org.example.rfshop.Auth.Domain.Dto.Request.ChangePasswordDto;
 import org.example.rfshop.Auth.Domain.Dto.Request.LoginDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final LoginUseCase loginUseCase;
+    private final ChangePasswordUseCase changePasswordUseCase;
 
     @Autowired
-    public AuthController(LoginUseCase loginUseCase) {
+    public AuthController(LoginUseCase loginUseCase, ChangePasswordUseCase changePasswordUseCase) {
         this.loginUseCase = loginUseCase;
+        this.changePasswordUseCase = changePasswordUseCase;
     }
 
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
         return new ResponseEntity<>( loginUseCase.execute(loginDto), HttpStatus.OK);
+    }
+
+    @PatchMapping("/change-password")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
+        this.changePasswordUseCase.execute(changePasswordDto);
+        return new ResponseEntity<>(  "Password update successfully",HttpStatus.OK);
     }
 }
