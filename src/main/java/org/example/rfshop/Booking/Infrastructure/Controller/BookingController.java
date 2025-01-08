9c5aa02e2d7c738c8524d3f15ae1b00fea0eec98
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-08 10:32:25 -0300

    feat: I defined booking controller

--- src/main/java/org/example/rfshop/Booking/Infrastructure/Controller/BookingController.java
+++ src/main/java/org/example/rfshop/Booking/Infrastructure/Controller/BookingController.java
@@ -1,9 +1,28 @@
 package org.example.rfshop.Booking.Infrastructure.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.rfshop.Booking.Application.GetBookingByBarberShopIdUseCase.GetBookingByBarberShopIdUseCase;
+import org.example.rfshop.Booking.Domain.Response.ListBookingResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/booking")
+@RequestMapping("/bookings")
 public class BookingController {
+
+    private final GetBookingByBarberShopIdUseCase   getBookingByBarberShopIdUseCase;
+
+
+    public BookingController(GetBookingByBarberShopIdUseCase getBookingByBarberShopIdUseCase) {
+        this.getBookingByBarberShopIdUseCase = getBookingByBarberShopIdUseCase;
+    }
+
+    @GetMapping("/barber-shop/{barberShopId}/month/{monthId}")
+    public ResponseEntity<?> getBookingsByBarberShopIdAndMonth(@PathVariable Long barberShopId, @PathVariable Long monthId) {
+        List<ListBookingResponseDto> bookings = getBookingByBarberShopIdUseCase.execute(barberShopId, monthId);
+        return new ResponseEntity<>(bookings, HttpStatus.OK);
+    }
+
 }
