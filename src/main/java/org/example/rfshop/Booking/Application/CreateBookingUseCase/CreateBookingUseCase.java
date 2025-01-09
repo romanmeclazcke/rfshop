d2ee17520c77d5620199ed69fca6498c6ab66df5
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-09 15:58:34 -0300

    feat: I implemented method to create a booking

--- src/main/java/org/example/rfshop/Booking/Application/CreateBookingUseCase/CreateBookingUseCase.java
+++ src/main/java/org/example/rfshop/Booking/Application/CreateBookingUseCase/CreateBookingUseCase.java
@@ -1,4 +1,10 @@
 package org.example.rfshop.Booking.Application.CreateBookingUseCase;
 
+import org.example.rfshop.Booking.Domain.Request.CreateBookingDto;
+import org.example.rfshop.Booking.Domain.Response.BookingResponseDto;
+
+import java.util.List;
+
 public interface CreateBookingUseCase {
+    List<BookingResponseDto> execute(Long barberShopId,List<CreateBookingDto> createBookingDto);
 }
--- src/main/java/org/example/rfshop/Booking/Domain/Request/CreateBookingDto.java
+++ src/main/java/org/example/rfshop/Booking/Domain/Request/CreateBookingDto.java
@@ -1,13 +1,22 @@
 package org.example.rfshop.Booking.Domain.Request;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateBookingDto {
-    private Long id;
+    @NotNull
+    private LocalDateTime bookingStart;
+
+    @NotNull
+    private LocalDateTime bookingEnd;
+
+    private Boolean reserved=false;
 }
