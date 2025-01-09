3158bee53ca4ae84ca3550b049ccb6d5ee69c63d
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-09 16:05:43 -0300

    feat: I implemented method to get all booking by barber shop id in a specific month

--- src/main/java/org/example/rfshop/Booking/Application/GetBookingByBarberShopIdUseCase/GetBookingByBarberShopIdUseCase.java
+++ src/main/java/org/example/rfshop/Booking/Application/GetBookingByBarberShopIdUseCase/GetBookingByBarberShopIdUseCase.java
@@ -1,9 +1,9 @@
 package org.example.rfshop.Booking.Application.GetBookingByBarberShopIdUseCase;
 
-import org.example.rfshop.Booking.Domain.Response.ListBookingResponseDto;
+import org.example.rfshop.Booking.Domain.Response.BookingResponseDto;
 
 import java.util.List;
 
 public interface GetBookingByBarberShopIdUseCase {
-    List<ListBookingResponseDto> execute(Long barberShopId, Long monthId);
+    List<BookingResponseDto> execute(Long barberShopId, Long monthId);
 }
--- src/main/java/org/example/rfshop/Booking/Application/GetBookingByBarberShopIdUseCase/GetBookingByBarberShopIdUseCaseImpl.java
+++ src/main/java/org/example/rfshop/Booking/Application/GetBookingByBarberShopIdUseCase/GetBookingByBarberShopIdUseCaseImpl.java
@@ -1,11 +1,14 @@
 package org.example.rfshop.Booking.Application.GetBookingByBarberShopIdUseCase;
 
-import org.example.rfshop.Booking.Domain.Response.ListBookingResponseDto;
+import org.example.rfshop.Booking.Domain.Response.BookingResponseDto;
 import org.example.rfshop.Booking.Infrastructure.Exception.InvalidMonth;
 import org.example.rfshop.Booking.Infrastructure.Mapper.BookingMapper;
 import org.example.rfshop.Booking.Infrastructure.Repository.BookingRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 
 @Service
@@ -20,14 +23,26 @@ public class GetBookingByBarberShopIdUseCaseImpl implements GetBookingByBarberSh
     }
 
     @Override
-    public List<ListBookingResponseDto> execute(Long barberShopId, Long monthId) {
+    public List<BookingResponseDto> execute(Long barberShopId, Long monthId) {
         try {
             if (monthId < 1 || monthId > 12) {
                 throw new InvalidMonth("Month must be between 1 and 12");
             }
-            return this.bookingRepository.getBookingByBarberShopIdAndMonth(barberShopId, monthId);
+            // current year
+            int currentYear = LocalDate.now().getYear();
+
+            // Range of dates
+            YearMonth yearMonth = YearMonth.of(currentYear, monthId.intValue());
+            LocalDateTime startDate = yearMonth.atDay(1).atStartOfDay(); // First day of the month
+            LocalDateTime endDate = yearMonth.atEndOfMonth().atTime(23, 59, 59); // Last day of the month
+
+            return this.bookingRepository.findBookingsByBarberShopAndMonth(barberShopId, startDate, endDate)
+                    .stream()
+                    .map(this.bookingMapper::toDto)
+                    .toList();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error while executing booking retrieval", e);
         }
     }
+
 }
