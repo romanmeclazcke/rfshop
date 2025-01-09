cade46f66fc172920853a88eda8888e0b451edaa
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-09 16:05:08 -0300

    feat: I implemented booking mapper and repository

--- src/main/java/org/example/rfshop/Booking/Infrastructure/Mapper/BookingMapper.java
+++ src/main/java/org/example/rfshop/Booking/Infrastructure/Mapper/BookingMapper.java
@@ -1,8 +1,13 @@
-//package org.example.rfshop.Booking.Infrastructure.Mapper;
-//
-//
-//import org.mapstruct.Mapper;
-//
-//@Mapper(componentModel = "spring")
-//public interface BookingMapper {
-//}
+package org.example.rfshop.Booking.Infrastructure.Mapper;
+
+import org.example.rfshop.Booking.Domain.Request.CreateBookingDto;
+import org.example.rfshop.Booking.Domain.Response.BookingResponseDto;
+import org.example.rfshop.Booking.Infrastructure.Model.Booking;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper {
+
+    Booking toEntity(CreateBookingDto createBookingDto);
+    BookingResponseDto toDto(Booking booking);
+}
