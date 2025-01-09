50e810a501565bf0d6d0859dc073e0353e0d9fc0
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-09 15:59:59 -0300

    feat: I updated booking controller to add new method (create, reserve and get booking by barber shop)

--- src/main/java/org/example/rfshop/Booking/Infrastructure/Controller/BookingController.java
+++ src/main/java/org/example/rfshop/Booking/Infrastructure/Controller/BookingController.java
@@ -1,7 +1,10 @@
 package org.example.rfshop.Booking.Infrastructure.Controller;
 
+import org.example.rfshop.Booking.Application.CreateBookingUseCase.CreateBookingUseCase;
 import org.example.rfshop.Booking.Application.GetBookingByBarberShopIdUseCase.GetBookingByBarberShopIdUseCase;
-import org.example.rfshop.Booking.Domain.Response.ListBookingResponseDto;
+import org.example.rfshop.Booking.Application.ReserveBookingUseCase.ReserveBookingUseCase;
+import org.example.rfshop.Booking.Domain.Request.CreateBookingDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,16 +16,29 @@ import java.util.List;
 public class BookingController {
 
     private final GetBookingByBarberShopIdUseCase   getBookingByBarberShopIdUseCase;
+    private final CreateBookingUseCase createBookingUseCase;
+    private final ReserveBookingUseCase reserveBookingUseCase;
 
-
-    public BookingController(GetBookingByBarberShopIdUseCase getBookingByBarberShopIdUseCase) {
+    @Autowired
+    public BookingController(GetBookingByBarberShopIdUseCase getBookingByBarberShopIdUseCase, CreateBookingUseCase createBookingUseCase, ReserveBookingUseCase reserveBookingUseCase) {
         this.getBookingByBarberShopIdUseCase = getBookingByBarberShopIdUseCase;
+        this.createBookingUseCase = createBookingUseCase;
+        this.reserveBookingUseCase = reserveBookingUseCase;
     }
 
     @GetMapping("/barber-shop/{barberShopId}/month/{monthId}")
     public ResponseEntity<?> getBookingsByBarberShopIdAndMonth(@PathVariable Long barberShopId, @PathVariable Long monthId) {
-        List<ListBookingResponseDto> bookings = getBookingByBarberShopIdUseCase.execute(barberShopId, monthId);
-        return new ResponseEntity<>(bookings, HttpStatus.OK);
+        return new ResponseEntity<>(getBookingByBarberShopIdUseCase.execute(barberShopId, monthId), HttpStatus.OK);
+    }
+
+    @PostMapping("/barber-shop/{barberShopId}")
+    public ResponseEntity<?> createBooking(@PathVariable Long barberShopId, @RequestBody List<CreateBookingDto> createBookingDto) {
+        return new ResponseEntity<>(this.createBookingUseCase.execute(barberShopId, createBookingDto),HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public ResponseEntity<?> reserveBooking(@PathVariable Long bookingId) {
+        return new ResponseEntity<>(this.reserveBookingUseCase.execute(bookingId),HttpStatus.OK);
     }
 
 }
