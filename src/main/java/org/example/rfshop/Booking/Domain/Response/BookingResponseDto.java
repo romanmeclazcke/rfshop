8784799a924de674e5304ce0f128f859b74534eb
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-09 16:04:32 -0300

    feat: I updated booking response dto (Added new attributes and json format)

--- src/main/java/org/example/rfshop/Booking/Domain/Response/BookingResponseDto.java
+++ src/main/java/org/example/rfshop/Booking/Domain/Response/BookingResponseDto.java
@@ -1,5 +1,6 @@
 package org.example.rfshop.Booking.Domain.Response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BookingResponseDto {
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime bookingStart;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime bookingEnd;
+    private Boolean reserved;
     private Boolean state;
     private UserNameDto user;
 }
