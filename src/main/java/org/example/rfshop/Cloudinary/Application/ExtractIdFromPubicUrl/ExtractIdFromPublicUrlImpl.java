14d9219992274d92a2d07a53efd750b81653bdd3
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2025-01-11 19:39:53 -0300

    fix: I changed method to extract public id from secure url

--- src/main/java/org/example/rfshop/Cloudinary/Application/ExtractIdFromPubicUrl/ExtractIdFromPublicUrlImpl.java
+++ src/main/java/org/example/rfshop/Cloudinary/Application/ExtractIdFromPubicUrl/ExtractIdFromPublicUrlImpl.java
@@ -1,15 +1,27 @@
 package org.example.rfshop.Cloudinary.Application.ExtractIdFromPubicUrl;
 
 import org.springframework.stereotype.Service;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ExtractIdFromPublicUrlImpl  implements  ExtractIdFromPublicUrl{
 
     @Override
-    public Optional<String> execute(String publicId) {
-         List<String> divider = List.of(publicId.split("/"));
-         return divider.getLast().split("\\.")[0].describeConstable();
+    public Optional<String> execute(String secureUrl) {
+        try {
+            String[] parts = secureUrl.split("/");
+
+            String folder = parts[parts.length - 2]; //folder name
+            String fileWithExtension = parts[parts.length - 1]; //public id and extension
+
+            String fileName = fileWithExtension.split("\\.")[0]; //remove extension
+
+            String publicId = folder + "/" + fileName; // concat folder name and public id
+
+            return Optional.of(publicId);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
+
 }
