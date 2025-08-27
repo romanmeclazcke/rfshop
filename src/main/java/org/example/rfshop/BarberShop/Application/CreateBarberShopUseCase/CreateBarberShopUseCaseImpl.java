adaaa1208258c8e1d80d8497bfa83e7861b0d16e
Roman Meclazcke <137325701+romanmeclazcke@users.noreply.github.com>
2025-08-27 14:00:49 -0300

    test

--- src/main/java/org/example/rfshop/BarberShop/Application/CreateBarberShopUseCase/CreateBarberShopUseCaseImpl.java
+++ src/main/java/org/example/rfshop/BarberShop/Application/CreateBarberShopUseCase/CreateBarberShopUseCaseImpl.java
@@ -34,16 +34,18 @@ public class CreateBarberShopUseCaseImpl implements CreateBarberShopUseCase {
 
     @Override
     public BarberShopResponseDto execute(CreateBarberShopDto createBarberShopDto) {
-        String email = this.extractUserEmailFromSecurityContext.execute(SecurityContextHolder.getContext());
-        User user = this.getUserByEmail.execute(email);
+    
+        import org.example.rfshop.Auth.Application.ExtractUserEmailFromSecurityContextUseCase.ExtractUserEmailFromSecurityContext;
+import org.example.rfshop.BarberShop.Domain.Dto.Request.CreateBarberShopDto;
+import org.example.rfshop.BarberShop.Domain.Dto.Response.BarberShopResponseDto;
+import org.example.rfshop.BarberShop.Infrastructure.Exception.InvalidRole;
+import org.example.rfshop.BarberShop.Infrastructure.Mapper.BarberShopMapper;
+import org.example.rfshop.BarberShop.Infrastructure.Model.BarberShop;
+import org.example.rfshop.BarberShop.Infrastructure.Repository.BarberShopRepository;
+import org.example.rfshop.User.Application.GetUserByEmail.GetUserByEmail;
+import org.example.rfshop.User.Infrastructure.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
-        if (!"BARBER".equals(user.getRole().getName())) {
-            throw new InvalidRole("If the user doesn't have role Barber, they cannot create a BarberShop");
-        }
-
-        BarberShop barberShop = this.barberShopMapper.toEntity(createBarberShopDto);
-        barberShop.setOwner(user);
-        this.barberShopRepository.save(barberShop);
-        return this.barberShopMapper.toDto(barberShop);
-    }
 }
